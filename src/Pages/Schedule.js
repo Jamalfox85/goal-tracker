7081dedcbeef02128692e8227037f1b83e3181c9
Jamalfox85 <Fox.Jamal@outlook.com>
2021-10-15 23:27:38 -0400

    Added schedule functionality. System will auto generate a list of 12 milestones and 12 dates for the user to download/print

--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -2,25 +2,95 @@ import React from 'react'
 
 const Schedule = () => {
 
+    // const goalLength = 12;
+    // const d1 = new Date()
+    // d1.setMonth(d1.getMonth()+ goalLength)
+
+    const setMilestoneDate = (milestone) => {
+        const milestoneDate = new Date();
+        milestoneDate.setMonth(milestoneDate.getMonth() + milestone)
+        return milestoneDate.toDateString();
+    }
+
     const schedule = [
 
         {
             goal: "Run more",
-            deadline: "December 31st",
+            milestone: 1,
+            deadline: setMilestoneDate(1),
             completed: <input type="checkbox"/>
         },
         {
             goal: "Read more",
-            deadline: "December 21st",
+            milestone: 2,
+            deadline: setMilestoneDate(2),
             completed: <input type="checkbox"/>
         },
         {
             goal: "Run more",
-            deadline: "December 31st",
+            milestone: 3,
+            deadline: setMilestoneDate(3),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 4,
+            deadline: setMilestoneDate(4),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 5,
+            deadline: setMilestoneDate(5),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Read more",
+            milestone: 6,
+            deadline: setMilestoneDate(6),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 7,
+            deadline: setMilestoneDate(7),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 8,
+            deadline: setMilestoneDate(8),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 9,
+            deadline: setMilestoneDate(9),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Read more",
+            milestone: 10,
+            deadline: setMilestoneDate(10),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 11,
+            deadline: setMilestoneDate(11),
+            completed: <input type="checkbox"/>
+        },
+        {
+            goal: "Run more",
+            milestone: 12,
+            deadline: setMilestoneDate(12),
             completed: <input type="checkbox"/>
         }
 
     ]
+
+
+    
 
     return (
         <div>
@@ -28,14 +98,16 @@ const Schedule = () => {
                 <thead>
                     <tr>
                         <th>Goal</th>
+                        <th>Milestone</th>
                         <th>Deadline</th>
                         <th>Completed?</th>
                     </tr>
                 </thead>
                 <tbody>
                     {schedule.map((item, index) => (
-                        <tr>
+                        <tr key={index}>
                             <td>{schedule[index].goal}</td>
+                            <td>{schedule[index].milestone}</td>
                             <td>{schedule[index].deadline}</td>
                             <td>{schedule[index].completed}</td>
                         </tr>
